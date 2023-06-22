cf6c1e4164e84b42ca0a59a522d3dbf9cad352e1
Abdelrahman.elkotb <abdelrahman.elkotb@etisalat.com>
2023-06-23 00:33:45 +0300

    editing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,37 +18,120 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _result= true;
+  bool _isRooted = true,
+      _isDev = false,
+      _isExtStorage = false,
+      _isEmulator = false;
 
   @override
   void initState() {
     super.initState();
     isRooted();
+    isExtStorage();
+    isEmulator();
+    isDevelopment();
   }
 
   Future<void> isRooted() async {
     try {
-      bool result = Platform.isAndroid ? await SecurityPlus.isRooted : await SecurityPlus.isJailBroken;
-      _result = result;
-    }catch (e){
-      log('\x1B[31m=====error: Security Breached======\x1B[0m');
+      bool result = Platform.isAndroid
+          ? await SecurityPlus.isRooted
+          : await SecurityPlus.isJailBroken;
+      _isRooted = result;
+    } catch (e) {
+      log('\x1B[31m${"=====error======"}\x1B[0m');
     }
 
-    setState(() {
-    });
+    setState(() {});
+  }
+
+  Future<void> isExtStorage() async {
+    if (Platform.isAndroid) {
+      try {
+        bool result = await SecurityPlus.isOnExternalStorage;
+        _isExtStorage = result;
+      } catch (e) {
+        log('\x1B[31m${"=====error======"}\x1B[0m');
+      }
+    }
+
+    setState(() {});
+  }
+
+  Future<void> isEmulator() async {
+    if (Platform.isAndroid) {
+      try {
+        bool result = await SecurityPlus.isEmulator;
+        _isEmulator = result;
+      } catch (e) {
+        log('\x1B[31m${"=====error======"}\x1B[0m');
+      }
+    }
+
+    setState(() {});
+  }
+
+  Future<void> isDevelopment() async {
+    if (Platform.isAndroid) {
+      try {
+        bool result = await SecurityPlus.isDevelopmentModeEnable;
+        _isDev = result;
+      } catch (e) {
+        log('\x1B[31m${"=====error======"}\x1B[0m');
+      }
+    }
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Rooted || JailBroken'),
         ),
-        body: Center(
-          child: Platform.isAndroid
-              ? Text('Android is rooted : $_result\n')
-              : Text('iOS is is jail broken : $_result\n'),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: Center(
+                  child: Platform.isAndroid
+                      ? Text('Android is rooted : $_isRooted\n')
+                      : Text('iOS is is jail broken : $_isRooted\n'),
+                ),
+              ),
+              if (Platform.isAndroid)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  child: Center(
+                      child:
+                          Text('Android is Emulator device : $_isEmulator\n')),
+                ),
+              if (Platform.isAndroid)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  child: Center(
+                      child: Text('Android is Development mode : $_isDev\n')),
+                ),
+              if (Platform.isAndroid)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  child: Center(
+                      child: Text(
+                          'Android is on External Storage : $_isExtStorage\n')),
+                ),
+            ],
+          ),
         ),
       ),
     );
