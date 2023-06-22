d30c903f6b379554157c2f25ddf67287583d2f34
Abdelrahman.elkotb <abdelrahman.elkotb@etisalat.com>
2023-06-23 01:00:10 +0300

    editing

--- lib/security_plus.dart
+++ lib/security_plus.dart
@@ -1,34 +1,40 @@
 
 import 'package:flutter/services.dart';
 
+/// A Flutter plugin to check for security alerts
 class SecurityPlus {
   static const MethodChannel _channel = MethodChannel('security_plus');
 
-  // Android Root checks
+  /// Android Root checks
+  /// this gets the value from native channel to flutter to know if phone is rooted
   static Future<bool> get isRooted async {
     final bool isRooted = await _channel.invokeMethod('isRooted');
     return isRooted;
   }
 
-  // iOS JailBroken checks
+  /// iOS JailBroken checks
+  /// this gets the value from native channel to flutter to know if phone is jail broken
   static Future<bool> get isJailBroken async {
     final bool isJailBroken = await _channel.invokeMethod('isJailBroken');
     return isJailBroken;
   }
 
-  // Android Emulator checks
+  /// Android Emulator checks
+  /// this gets the value from native channel to flutter to know if app is running on an Emulator
   static Future<bool> get isEmulator async {
     final bool isRooted = await _channel.invokeMethod('isEmulator');
     return isRooted;
   }
 
-  // Android on External Storage checks
+  /// Android on External Storage check
+  /// this gets the value from native channel to flutter to know if app is running on an external storage
   static Future<bool> get isOnExternalStorage async {
     final bool isRooted = await _channel.invokeMethod('isOnExternalStorage');
     return isRooted;
   }
 
-  // Android Development mode checks
+  /// Android Development mode check
+  /// this gets the value from native channel to flutter to know if app is running in development environment
   static Future<bool> get isDevelopmentModeEnable async {
     final bool isRooted = await _channel.invokeMethod('isDevelopmentModeEnable');
     return isRooted;
