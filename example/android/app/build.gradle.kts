a58a409e99eea8b565e27f36be39ecb1e9a66dd4
khaledabdelnasser-etisalat <khaled.abdelnasser@etisalat.com>
2025-10-08 15:36:27 +0300

    google 16kb page requirement (#22)
    * google 16kb page requirement
    
    * fix dep issue
    
    * fix dep issue with permission_handler

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.security_plus_example"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
